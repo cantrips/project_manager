78eb26fa9ebbdba62cddc0dab4937ed66e3976e2
John <john@Johns-MacBook-Air.local>
2018-12-01 18:52:41 -0500

    initial commit

---
+++ src/main/java/io/ueb/projectlist/ProjectListApplication.java
@@ -0,0 +1,12 @@
+package io.ueb.projectlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectListApplication.class, args);
+	}
+}
---
+++ src/test/java/io/ueb/projectlist/ProjectListApplicationTests.java
@@ -0,0 +1,16 @@
+package io.ueb.projectlist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectListApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
